0dcc297d303abe2c8cf2f9a17a40fc9c92afa2b8
Huikie <z4everaz@hotmail.com>
2018-11-17 13:06:55 +0100

    Main Activity & Choose_txt activity & possibility to load and show text from text file

---
+++ app/src/main/java/com/example/daan/daan_huikeshoven_pset2/Choose_txt.java
@@ -0,0 +1,20 @@
+package com.example.daan.daan_huikeshoven_pset2;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class Choose_txt extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_choose_txt);
+    }
+    //Possibility to load text and put it in a textview
+
+    //InputStream is = getResources().openRawResource(R.raw.madlib0_simple);
+    //Story text = new Story(is);
+    //String text1 = text.toString();
+    //TextView text2 = findViewById(R.id.text);
+    //text2.setText(text1);
+}
--- app/src/main/java/com/example/daan/daan_huikeshoven_pset2/MainActivity.java
+++ app/src/main/java/com/example/daan/daan_huikeshoven_pset2/MainActivity.java
@@ -2,6 +2,10 @@ package com.example.daan.daan_huikeshoven_pset2;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
+import android.widget.TextView;
+
+import java.io.InputStream;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,4 +14,5 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
     }
+
 }
